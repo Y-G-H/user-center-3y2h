dd750c2c6f4e0ab1325f34050c6c7564441b0188
孔明 <wb-yanganghui@cai-inc.com>
2020-11-11 23:51:40 +0800

    更新配置项

--- user-center-3y2h-web/src/main/java/cn/y3h2/blog/user/web/DubboStarter.java
+++ user-center-3y2h-web/src/main/java/cn/y3h2/blog/user/web/DubboStarter.java
@@ -1,6 +1,6 @@
 package cn.y3h2.blog.user.web;
 
-import com.alibaba.dubbo.spring.boot.annotation.EnableDubboConfiguration;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +8,13 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {
+        "cn.y3h2.blog.user.web",
         "cn.y3h2.blog.user.provider",
+        "cn.y3h2.blog.user.core",
 })
 @Configuration
 @EnableScheduling
+@MapperScan("cn.y3h2.blog.user.core.dao")
 @ImportResource("classpath:/spring/dubbo.xml")
 public class DubboStarter {
 
