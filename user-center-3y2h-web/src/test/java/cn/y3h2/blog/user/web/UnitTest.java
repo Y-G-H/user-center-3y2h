e0154ad419ce17651ba3cf9f46ec0b7bc2e25d39
孔明 <wb-yanganghui@cai-inc.com>
2020-11-11 23:52:04 +0800

    单元测试

---
+++ user-center-3y2h-web/src/test/java/cn/y3h2/blog/user/web/UnitTest.java
@@ -0,0 +1,30 @@
+package cn.y3h2.blog.user.web;
+
+import cn.y3h2.blog.user.api.domain.FindUserCondition;
+import cn.y3h2.blog.user.core.domain.UsrUserDO;
+import cn.y3h2.blog.user.core.manager.UsrUserManager;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DubboStarter.class)
+public class UnitTest {
+
+    @Autowired
+    private UsrUserManager usrUserManager;
+
+    @Test
+    public void test() {
+        FindUserCondition findUserCondition = new FindUserCondition();
+        findUserCondition.setRealname("颜港辉");
+
+        List<UsrUserDO> load = usrUserManager.load(findUserCondition);
+        System.out.println(load);
+    }
+
+}
